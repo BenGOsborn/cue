d12b90f6afc20a3f990fcd4405e0ed2ffe4ee441
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-06-17 06:12:59 +1000

    Added plan for efficient scaling

--- src/gateway/main.go
+++ src/gateway/main.go
@@ -83,6 +83,11 @@ func main() {
 	// into another channel for where the message should go and what category it should be assigned ??? (this needs to be scalable)
 	// Raw messagw from service -> message queue processing channel -> processed ready to be sent to message channel / broadcast to user channel / broadcast to all chanel
 
+	// **** For efficiency, we will have an "express" Kafka channel which when the message is sent this server id is attached to it, and then when the message is sent back
+	// it will go directly to a queue only listened to by this server which will process it - if the message does not belong to this user, we will add it to a global queue
+	// which is listened to by all servers.
+	// Also remove the broadcast to all - it will NOT work in a distributed environment and is not needed.
+
 	for i := 0; i < 5; i++ {
 		go broadcastMsg(connections, messages)
 	}
