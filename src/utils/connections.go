af5238e1b74833df1d2c8a7d6e08087856a76a1c
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-06-17 05:49:58 +1000

    Fixed mutex not ever being declared bug and updated to use reader / writer lock

--- src/utils/connections.go
+++ src/utils/connections.go
@@ -9,20 +9,48 @@ import (
 
 type Connections struct {
 	connections map[string]*websocket.Conn
-	mutex       map[string]*sync.Mutex
+	mutex       map[string]*sync.RWMutex
 }
 
 // Create a new connections struct
 func NewConnections() *Connections {
-	return &Connections{connections: make(map[string]*websocket.Conn), mutex: make(map[string]*sync.Mutex)}
+	return &Connections{connections: make(map[string]*websocket.Conn), mutex: make(map[string]*sync.RWMutex)}
 }
 
-// Lock the mutex
-func (connections *Connections) lock(id string) error {
+// Lock the mutex for reading
+func (connections *Connections) lockRead(id string) error {
+	lock, ok := connections.mutex[id]
+
+	if !ok {
+		lock = &sync.RWMutex{}
+		connections.mutex[id] = lock
+	}
+
+	lock.RLock()
+
+	return nil
+}
+
+// Unlock the mutex for reading
+func (connections *Connections) unlockRead(id string) error {
 	lock, ok := connections.mutex[id]
 
 	if !ok {
 		return errors.New("No lock with this id exists.")
+	}
+
+	lock.RUnlock()
+
+	return nil
+}
+
+// Lock the mutex for writing
+func (connections *Connections) lockWrite(id string) error {
+	lock, ok := connections.mutex[id]
+
+	if !ok {
+		lock = &sync.RWMutex{}
+		connections.mutex[id] = lock
 	}
 
 	lock.Lock()
@@ -30,8 +58,8 @@ func (connections *Connections) lock(id string) error {
 	return nil
 }
 
-// Unlock the mutex
-func (connections *Connections) unlock(id string) error {
+// Unlock the mutex for writing
+func (connections *Connections) unlockWrite(id string) error {
 	lock, ok := connections.mutex[id]
 
 	if !ok {
@@ -45,24 +73,24 @@ func (connections *Connections) unlock(id string) error {
 
 // Add a new connection
 func (connections *Connections) Add(id string, conn *websocket.Conn) {
-	connections.lock(id)
-	defer connections.unlock(id)
+	connections.lockWrite(id)
+	defer connections.unlockWrite(id)
 
 	connections.connections[id] = conn
 }
 
 // Remove a connection
 func (connections *Connections) Remove(id string) {
-	connections.lock(id)
-	defer connections.unlock(id)
+	connections.lockWrite(id)
+	defer connections.unlockWrite(id)
 
 	delete(connections.connections, id)
 }
 
 // Check if a connection exists
 func (connections *Connections) Exists(id string) bool {
-	connections.lock(id)
-	defer connections.unlock(id)
+	connections.lockRead(id)
+	defer connections.unlockRead(id)
 
 	_, ok := connections.connections[id]
 
@@ -72,8 +100,8 @@ func (connections *Connections) Exists(id string) bool {
 // Apply a function to all connections
 func (connections *Connections) ForEach(fn func(string, *websocket.Conn) error) error {
 	for id, conn := range connections.connections {
-		connections.lock(id)
-		defer connections.unlock(id)
+		connections.lockRead(id)
+		defer connections.unlockRead(id)
 
 		if err := fn(id, conn); err != nil {
 			return err
@@ -85,8 +113,8 @@ func (connections *Connections) ForEach(fn func(string, *websocket.Conn) error) 
 
 // Apply a function to a particular connection
 func (connections *Connections) Apply(id string, fn func(string, *websocket.Conn) error) (bool, error) {
-	connections.lock(id)
-	defer connections.unlock(id)
+	connections.lockRead(id)
+	defer connections.unlockRead(id)
 
 	conn, ok := connections.connections[id]
 
