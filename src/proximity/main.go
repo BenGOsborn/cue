18cd5d86c3906fcf3eeddf55cbfc1c9ccc2ba7d2
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-06-23 22:11:14 +1000

    Partition works

--- src/proximity/main.go
+++ src/proximity/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/bengosborn/cue/proximity/utils"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	var longitude float32 = -60.0
+	var latitude float32 = 20.0
+
+	partition, err := utils.NewPartition(latitude, longitude)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(partition)
+	}
 }
--- src/proximity/utils/partition.go
+++ src/proximity/utils/partition.go
@@ -13,14 +13,14 @@ type Partition struct {
 // Depth of the partitioning e.g. the size of each partition
 const (
 	PartitionDepth = 10
-	LongitudeMin   = -180
-	LongitudeMax   = 180
 	LatitudeMin    = -90
 	LatitudeMax    = 90
+	LongitudeMin   = -180
+	LongitudeMax   = 180
 )
 
 // Calculate a partition string with a given depth from a given latitude and longitude
-func calculatePartition(latitude float32, longitude float32, longitudeMin float32, longitudeMax float32, latitudeMin float32, latitudeMax float32, depth uint) (string, error) {
+func calculatePartition(latitude float32, longitude float32, latitudeMin float32, latitudeMax float32, longitudeMin float32, longitudeMax float32, depth uint) (string, error) {
 	// Base case
 	if depth == 0 {
 		return "", nil
@@ -30,23 +30,7 @@ func calculatePartition(latitude float32, longitude float32, longitudeMin float3
 		return "", errors.New("out of bounds")
 	}
 
-	// Recursive partitioning longitude
-	midLong := (longitudeMin + longitudeMax) / 2
-
-	var newLongMin float32
-	var newLongMax float32
-
 	id := 0
-
-	if longitude < midLong {
-		id += 0
-		newLongMin = longitudeMin
-		newLongMax = midLong
-	} else {
-		id += 1
-		newLongMin = midLong
-		newLongMax = longitudeMax
-	}
 
 	// Recursive partitioning latitude
 	midLat := (latitudeMin + latitudeMax) / 2
@@ -59,12 +43,28 @@ func calculatePartition(latitude float32, longitude float32, longitudeMin float3
 		newLatMin = latitudeMin
 		newLatMax = midLat
 	} else {
-		id += 2
+		id += 1
 		newLatMin = midLat
 		newLatMax = latitudeMax
 	}
 
-	suffix, err := calculatePartition(latitude, longitude, newLongMin, newLongMax, newLatMin, newLatMax, depth-1)
+	// Recursive partitioning longitude
+	midLong := (longitudeMin + longitudeMax) / 2
+
+	var newLongMin float32
+	var newLongMax float32
+
+	if longitude < midLong {
+		id += 0
+		newLongMin = longitudeMin
+		newLongMax = midLong
+	} else {
+		id += 2
+		newLongMin = midLong
+		newLongMax = longitudeMax
+	}
+
+	suffix, err := calculatePartition(latitude, longitude, newLatMin, newLatMax, newLongMin, newLongMax, depth-1)
 	if err != nil {
 		return "", err
 	}
@@ -74,7 +74,7 @@ func calculatePartition(latitude float32, longitude float32, longitudeMin float3
 
 // Create a new partition from a latitude and longitude
 func NewPartition(latitude float32, longitude float32) (*Partition, error) {
-	encoded, err := calculatePartition(latitude, longitude, LongitudeMin, LongitudeMax, LatitudeMin, LatitudeMax, PartitionDepth)
+	encoded, err := calculatePartition(latitude, longitude, LatitudeMin, LatitudeMax, LongitudeMin, LongitudeMax, PartitionDepth)
 	if err != nil {
 		return nil, err
 	}
