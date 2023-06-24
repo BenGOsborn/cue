d2c630a6f9ab5e1ac0f0b8746d215e64e25b0b14
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-06-24 12:06:01 +1000

    Added chunk as partition

--- src/proximity/utils/partition.go
+++ src/proximity/utils/partition.go
@@ -6,8 +6,14 @@ import (
 	"strings"
 )
 
+type Chunk struct {
+	right bool
+	down  bool
+}
+
 type Partition struct {
 	encoded string
+	chunks  *[]*Chunk
 }
 
 // Depth of the partitioning e.g. the size of each partition
@@ -19,67 +25,99 @@ const (
 	LongitudeMax   = 180
 )
 
-// Calculate a partition string with a given depth from a given latitude and longitude
-func calculatePartition(latitude float32, longitude float32, latitudeMin float32, latitudeMax float32, longitudeMin float32, longitudeMax float32, depth uint) (string, error) {
-	// Base case
-	if depth == 0 {
-		return "", nil
+// Partition a given latitude and longitude
+func partition(latitude float32, longitude float32, latitudeMin float32, latitudeMax float32, longitudeMin float32, longitudeMax float32, depth uint) (string, *[]*Chunk, error) {
+	buffer := strings.Builder{}
+	chunks := make([]*Chunk, depth)
+
+	var recurse func(float32, float32, float32, float32, uint) error
+	recurse = func(latitudeMin float32, latitudeMax float32, longitudeMin float32, longitudeMax float32, depth uint) error {
+		// Base case
+		if depth <= 0 {
+			return nil
+		}
+
+		if latitude < latitudeMin || latitude > latitudeMax || longitude < longitudeMin || longitude > longitudeMax {
+			return errors.New("out of bounds")
+		}
+
+		// Recursive partitioning latitude
+		midLat := (latitudeMin + latitudeMax) / 2
+
+		down := false
+		var newLatMin float32
+		var newLatMax float32
+
+		if latitude < midLat {
+			newLatMin = latitudeMin
+			newLatMax = midLat
+		} else {
+			down = true
+			newLatMin = midLat
+			newLatMax = latitudeMax
+		}
+
+		// Recursive partitioning longitude
+		midLong := (longitudeMin + longitudeMax) / 2
+
+		right := false
+		var newLongMin float32
+		var newLongMax float32
+
+		if longitude < midLong {
+			newLongMin = longitudeMin
+			newLongMax = midLong
+		} else {
+			right = true
+			newLongMin = midLong
+			newLongMax = longitudeMax
+		}
+
+		// Write the data
+		depth -= 1
+		chunks[depth] = &Chunk{down: down, right: right}
+
+		downDigit := "0"
+		if down {
+			downDigit = "1"
+		}
+		if _, err := buffer.WriteString(downDigit); err != nil {
+			return err
+		}
+
+		rightDigit := "0"
+		if right {
+			rightDigit = "1"
+		}
+		if _, err := buffer.WriteString(rightDigit); err != nil {
+			return err
+		}
+
+		if _, err := buffer.WriteString(" "); err != nil {
+			return err
+		}
+
+		return recurse(newLatMin, newLatMax, newLongMin, newLongMax, depth)
 	}
 
-	if latitude < latitudeMin || latitude > latitudeMax || longitude < longitudeMin || longitude > longitudeMax {
-		return "", errors.New("out of bounds")
+	if err := recurse(latitudeMin, latitudeMax, longitudeMin, longitudeMax, depth); err != nil {
+		return "", nil, err
 	}
 
-	id := 0
-
-	// Recursive partitioning latitude
-	midLat := (latitudeMin + latitudeMax) / 2
-
-	var newLatMin float32
-	var newLatMax float32
-
-	if latitude < midLat {
-		id += 0
-		newLatMin = latitudeMin
-		newLatMax = midLat
-	} else {
-		id += 1
-		newLatMin = midLat
-		newLatMax = latitudeMax
-	}
-
-	// Recursive partitioning longitude
-	midLong := (longitudeMin + longitudeMax) / 2
-
-	var newLongMin float32
-	var newLongMax float32
-
-	if longitude < midLong {
-		id += 0
-		newLongMin = longitudeMin
-		newLongMax = midLong
-	} else {
-		id += 2
-		newLongMin = midLong
-		newLongMax = longitudeMax
-	}
-
-	suffix, err := calculatePartition(latitude, longitude, newLatMin, newLatMax, newLongMin, newLongMax, depth-1)
-	if err != nil {
-		return "", err
-	}
-
-	return fmt.Sprint(id, suffix), nil
+	return buffer.String(), &chunks, nil
 }
 
 // Create a new partition from a latitude and longitude
 func NewPartition(latitude float32, longitude float32) (*Partition, error) {
-	encoded, err := calculatePartition(latitude, longitude, LatitudeMin, LatitudeMax, LongitudeMin, LongitudeMax, PartitionDepth)
+	encoded, chunks, err := partition(latitude, longitude, LatitudeMin, LatitudeMax, LongitudeMin, LongitudeMax, PartitionDepth)
 	if err != nil {
 		return nil, err
 	}
 
-	return &Partition{encoded: encoded}, nil
+	fmt.Println(encoded)
+	fmt.Println(chunks)
+
+	return &Partition{encoded: encoded, chunks: chunks}, nil
 }
 
 // Format the partition
@@ -92,7 +130,16 @@ func (p *Partition) Contains(partition *Partition) bool {
 	return strings.Contains(partition.encoded, p.encoded)
 }
 
+// Translate a partition string in some direction
+func translate() {
+
+}
+
 // Find all surrounding partitions e.g. the surrounding 8 items
 func (p *Partition) Surrounding() {
 	// **** I do not know what I am doing here
+
+	// **** Ground rules - if we need to go back from 0 we need to go to the previous, we rotate back to the end of the string in our search
+
+	// **** We actually need a trie structure for this instead
 }
