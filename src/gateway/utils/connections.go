7aebd279bc24f990ec43fdff8db8ac9919f0b928
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-06-25 16:24:32 +1000

    Updated connections to be threadsafe

--- src/gateway/utils/connections.go
+++ src/gateway/utils/connections.go
@@ -1,28 +1,36 @@
 package gateway
 
 import (
+	"sync"
+
 	"github.com/bengosborn/cue/utils"
 	"github.com/gorilla/websocket"
 )
 
 type Connections struct {
-	connections map[string]*websocket.Conn
+	connections sync.Map
 	lock        *utils.ResourceLock
 }
 
 // Create a new connections struct
 func NewConnections() *Connections {
-	return &Connections{connections: make(map[string]*websocket.Conn), lock: utils.NewResourceLock()}
+	return &Connections{connections: sync.Map{}, lock: utils.NewResourceLock()}
 }
 
 // Close all connections
 func (c *Connections) Close() {
-	for id, conn := range c.connections {
+	c.connections.Range(func(key, value interface{}) bool {
+		id := key.(string)
+
 		c.lock.LockWrite(id)
 		defer c.lock.UnlockWrite(id)
 
+		conn := value.(*websocket.Conn)
+
 		conn.Close()
-	}
+
+		return true
+	})
 }
 
 // Add a new connection
@@ -30,7 +38,7 @@ func (c *Connections) Add(id string, conn *websocket.Conn) {
 	c.lock.LockWrite(id)
 	defer c.lock.UnlockWrite(id)
 
-	c.connections[id] = conn
+	c.connections.Store(id, conn)
 }
 
 // Remove a connection
@@ -38,8 +46,15 @@ func (c *Connections) Remove(id string) {
 	c.lock.LockWrite(id)
 	defer c.lock.UnlockWrite(id)
 
-	c.connections[id].Close()
-	delete(c.connections, id)
+	value, ok := c.connections.Load(id)
+	if !ok {
+		return
+	}
+
+	conn := value.(*websocket.Conn)
+	conn.Close()
+
+	c.connections.Delete(id)
 }
 
 // Check if a connection exists
@@ -47,35 +62,38 @@ func (c *Connections) Exists(id string) bool {
 	c.lock.LockRead(id)
 	defer c.lock.UnlockRead(id)
 
-	_, ok := c.connections[id]
-
+	_, ok := c.connections.Load(id)
 	return ok
 }
 
 // Apply a function to all connections
-func (c *Connections) ForEach(fn func(string, *websocket.Conn) error) error {
-	for id, conn := range c.connections {
+func (c *Connections) ForEach(fn func(string, *websocket.Conn) error) {
+	c.connections.Range(func(key, value any) bool {
+		id := key.(string)
+		conn := value.(*websocket.Conn)
+
 		c.lock.LockRead(id)
 		defer c.lock.UnlockRead(id)
 
 		if err := fn(id, conn); err != nil {
-			return err
+			return false
 		}
-	}
 
-	return nil
+		return true
+	})
 }
 
 // Apply a function to a particular connection
 func (c *Connections) Apply(id string, fn func(string, *websocket.Conn) error) (bool, error) {
-	c.lock.LockRead(id)
-	defer c.lock.UnlockRead(id)
-
-	conn, ok := c.connections[id]
-
+	value, ok := c.connections.Load(id)
 	if !ok {
 		return false, nil
 	}
+
+	conn := value.(*websocket.Conn)
+
+	c.lock.LockRead(id)
+	defer c.lock.UnlockRead(id)
 
 	if err := fn(id, conn); err != nil {
 		return false, err
--- src/proximity/utils/location.go
+++ src/proximity/utils/location.go
@@ -1,9 +1,12 @@
 package utils
 
 import (
-	"errors"
+	"context"
 	"sync"
 	"time"
+
+	"github.com/bengosborn/cue/utils"
+	"github.com/redis/go-redis/v9"
 )
 
 type UserData struct {
@@ -13,22 +16,32 @@ type UserData struct {
 }
 
 type Location struct {
-	lock     sync.RWMutex
-	location map[string]map[string]*UserData // partition encoded -> user -> user data
-	user     map[string]*Partition           // user -> partition
+	redis    *redis.Client
+	lock     *utils.ResourceLockDistributed
+	mutex    sync.RWMutex
+	location sync.Map // partition encoded -> user -> user data
+	user     sync.Map // user -> partition
 }
 
-// **** This will need to work with Redis instead to make it completely distributed
+const (
+	timeout = 5 * time.Minute
+)
+
+// **** I need to use this with a distributed lock and redis...
+// 1. When reading, we need to look at redis values and the values we have locally and choose the one with the most recent timestamp
+// 2. When writing, we will have a method to sync the local data with what is in the redis database, where it will only update the partition with the latest timestamp
 
 // Make a new location structure
-func NewLocation() *Location {
-	return &Location{location: make(map[string]map[string]*UserData), user: make(map[string]*Partition), lock: sync.RWMutex{}}
+func NewLocation(ctx context.Context, redis *redis.Client, lock *utils.ResourceLockDistributed) *Location {
+	return &Location{location: sync.Map{}, user: sync.Map{}, mutex: sync.RWMutex{}, redis: redis, lock: lock}
 }
 
 // Add a new user
 func (u *Location) Upsert(user string, lat float32, long float32) error {
-	u.lock.Lock()
-	defer u.lock.Unlock()
+	u.mutex.Lock()
+	defer u.mutex.Unlock()
+
+	// **** We do know that the partition will indeed be a single map -> do something with this information
 
 	// Remove the user from the previous partition if they already exist
 	prev, ok := u.user[user]
@@ -51,48 +64,48 @@ func (u *Location) Upsert(user string, lat float32, long float32) error {
 	return nil
 }
 
-// Remove a user
-func (u *Location) Remove(user string) error {
-	u.lock.Lock()
-	defer u.lock.Unlock()
+// // Remove a user
+// func (u *Location) Remove(user string) error {
+// 	u.mutex.Lock()
+// 	defer u.mutex.Unlock()
 
-	// Remove user
-	prev, ok := u.user[user]
-	if !ok {
-		return errors.New("user does not exist")
-	}
+// 	// Remove user
+// 	prev, ok := u.user[user]
+// 	if !ok {
+// 		return errors.New("user does not exist")
+// 	}
 
-	delete(u.user, user)
-	delete(u.location[prev.encoded], user)
+// 	delete(u.user, user)
+// 	delete(u.location[prev.encoded], user)
 
-	return nil
-}
+// 	return nil
+// }
 
-// Get nearby users
-func (u *Location) Nearby(user string, radius int) ([]string, error) {
-	u.lock.RLock()
-	defer u.lock.RUnlock()
+// // Get nearby users
+// func (u *Location) Nearby(user string, radius int) ([]string, error) {
+// 	u.mutex.RLock()
+// 	defer u.mutex.RUnlock()
 
-	// Get the partition for the user
-	userPartition, ok := u.user[user]
-	if !ok {
-		return nil, errors.New("user does not exist")
-	}
+// 	// Get the partition for the user
+// 	userPartition, ok := u.user[user]
+// 	if !ok {
+// 		return nil, errors.New("user does not exist")
+// 	}
 
-	// Get the nearby partitions and find all users
-	partitions, err := userPartition.Nearby(radius)
-	if err != nil {
-		return nil, err
-	}
+// 	// Get the nearby partitions and find all users
+// 	partitions, err := userPartition.Nearby(radius)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	users := make([]string, 0)
-	for _, partition := range *partitions {
-		for usr := range u.location[partition.encoded] {
-			if usr != user {
-				users = append(users, usr)
-			}
-		}
-	}
+// 	users := make([]string, 0)
+// 	for _, partition := range *partitions {
+// 		for usr := range u.location[partition.encoded] {
+// 			if usr != user {
+// 				users = append(users, usr)
+// 			}
+// 		}
+// 	}
 
-	return users, nil
-}
+// 	return users, nil
+// }
